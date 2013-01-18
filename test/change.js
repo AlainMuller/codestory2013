4331609034a5f011e2061c5a0a78a33e4da5e70b
Alain Muller <alain.muller@gmail.com>
2013-01-18 23:03:47 +0100

    Quelques premiers tests avec mocha et should

--- test/change.js
+++ test/change.js
@@ -1,12 +1,31 @@
 var should = require('should');
-var change = require('../lib/change');
+var change = require(__dirname + '/../lib/change');
 
 describe('change', function() {
-    describe('with no arguments', function() {
-        it('returns an empty array', function() {
-        	// On a exporté la méthode, on peut l'appeler directement!
-            var result = change();
-            result.should.eql([]);
+    describe('#paramètres', function() {
+        it('tableau vide si pas de paramètre', function() {
+            change().should.eql([]);
         });
+        it('tableau vide sur paramètres multiples', function() {
+    		var foo, bar;
+    		change(foo, bar).should.eql([]);
+    	});
+    });
+
+    describe('#chiffre', function() {
+    	it('#0 returns []', function(){
+    		change(0).should.eql([]);
+    	});
+    	it('#1 returns [{"foo":1}]', function(){
+    		change(1).should.eql([{"foo":1}]);
+    	});
+    	it('#2 returns [{"foo":2}]');
+    	it('#3 returns [{"foo":3}]');
+    	it('#4 returns [{"foo":4}]');
+    	it('#5 returns [{"foo":5}]');
+    	it('#6 returns [{"foo":6}]');
+    	it('#7 returns [{"foo":7}, {"bar":1}]');
+    	it('#8 returns [{"foo":8}, {"foo":1, "bar":1}]');
+    	it('#9 returns [{"foo":2, "bar":1}]');
     });
 });
