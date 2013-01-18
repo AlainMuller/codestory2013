a153e89cbdc967efd559a661fbab331a8db7e6ce
Alain Muller <alain.muller@gmail.com>
2013-01-18 23:46:29 +0100

    Algo bidon, fait planter les tests

--- test/change.js
+++ test/change.js
@@ -3,29 +3,47 @@ var change = require(__dirname + '/../lib/change');
 
 describe('change', function() {
     describe('#paramètres', function() {
-        it('tableau vide si pas de paramètre', function() {
-            change().should.eql([]);
-        });
-        it('tableau vide sur paramètres multiples', function() {
+        it('traite un seul paramètre', function() {
     		var foo, bar;
-    		change(foo, bar).should.eql([]);
+    		change.makeChange().should.eql([]);
+    		change.makeChange(foo, bar).should.eql([]);
     	});
+        it('traite un nombre', function() {
+        	var a = "plop", b = 1, c = {"foo":5};
+            change.makeChange(a).should.eql([]);
+            change.makeChange(b).should.not.eql([]);
+            change.makeChange(c).should.eql([]);
+
+            change.makeChange().should.eql([]);
+        });
     });
 
     describe('#chiffre', function() {
-    	it('#0 returns []', function(){
-    		change(0).should.eql([]);
+    	it('0 retourne []', function(){
+    		change.makeChange(0).should.eql([]);
     	});
-    	it('#1 returns [{"foo":1}]', function(){
-    		change(1).should.eql([{"foo":1}]);
+    	it('1 retourne [{"foo":1}]', function(){
+    		change.makeChange(1).should.eql([{"foo":1}]);
     	});
-    	it('#2 returns [{"foo":2}]');
-    	it('#3 returns [{"foo":3}]');
-    	it('#4 returns [{"foo":4}]');
-    	it('#5 returns [{"foo":5}]');
-    	it('#6 returns [{"foo":6}]');
-    	it('#7 returns [{"foo":7}, {"bar":1}]');
-    	it('#8 returns [{"foo":8}, {"foo":1, "bar":1}]');
-    	it('#9 returns [{"foo":2, "bar":1}]');
+    	it('2 retourne [{"foo":2}]', function(){
+    		change.makeChange(2).should.eql([{"foo":2}]);
+    	});
+    	it('3 retourne [{"foo":3}]', function(){
+    		change.makeChange(3).should.eql([{"foo":3}]);
+    	});
+    	it('4 retourne [{"foo":4}]', function(){
+    		change.makeChange(4).should.eql([{"foo":4}]);
+    	});
+    	it('5 retourne [{"foo":5}]', function(){
+    		change.makeChange(5).should.eql([{"foo":5}]);
+    	});
+    	it('6 retourne [{"foo":6}]', function(){
+    		change.makeChange(6).should.eql([{"foo":6}]);
+    	});
+    	it('7 retourne [{"foo":7}, {"bar":1}]', function(){
+    		change.makeChange(7).should.eql([{"foo":7, "bar":1}]);
+    	});
+    	it('8 retourne [{"foo":8}, {"foo":1, "bar":1}]');
+    	it('9 retourne [{"foo":2, "bar":1}]');
     });
 });
