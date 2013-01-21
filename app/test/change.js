351dec94b9a5c61fe95ff7d96a20938aed0df717
Alain Muller <alain.muller@gmail.com>
2013-01-21 10:31:15 +0100

    Ignored WebStorm project directory

--- app/test/change.js
+++ app/test/change.js
@@ -1,5 +1,5 @@
 var should = require('should');
-var change = require(__dirname + '/../lib/change');
+var change = require(__dirname + '/../app/lib/change');
 
 describe('change', function() {
     describe('#paramètres', function() {
