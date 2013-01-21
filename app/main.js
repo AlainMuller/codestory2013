c09731bfdbc323595a940173caf3396c25c25b1d
Alain Muller <alain.muller@gmail.com>
2013-01-21 20:51:58 +0100

    Using calcul.js module + french formatting 1.5 => 1,5

--- app/main.js
+++ app/main.js
@@ -15,6 +15,7 @@ var url = require("url");
 var change = require(__dirname + "/lib/change");
 var util = require(__dirname + "/lib/util");
 var routes = require(__dirname + "/lib/routes.js");
+var calc = require(__dirname + "/lib/calcul.js");
 
 // Chargement de l'objet JSON (association requête(URL) / réponse au format JSON)
 var queries = require(__dirname + "/res/queries.json");
@@ -32,173 +33,164 @@ var favicon;
 // Mise en place du serveur HTTP
 var server = http.createServer(function (request, response) {
 
-    util.log("Réception d'une requête : '" + request.url + "'");
-    // On trace la requête dans un fichier de log
-    util.logReq(request);
+            util.log("Réception d'une requête : '" + request.url + "'");
+            // On trace la requête dans un fichier de log
+            util.logReq(request);
 
-    // Requête POST
-    if (request.method == 'POST') {
-        var postRequest;
-        var body = '';
-        request.on('data', function (data) {
-            body += data;
-        });
-        request.on('end', function () {
-            postRequest = qs.parse(body);
-            util.log("Requête POST : " + postRequest);
-            util.log(" >> Data  : " + JSON.stringify(postRequest));
-            // Sauvegarde des données de la requête POST dans un fichier de log
-            util.logData(request, JSON.stringify(postRequest));
-
-            // TODO : stocker les données de l'énoncé au format MarkDown (le cas échéant) dans le fichier correspondant /res/enonceX.md
-        });
-
-        // Ecriture de l'En-tête HTTP (201 : Status CREATED)
-        response.writeHead(201, {"Content-Type":"text/plain;charset=utf-8"});
-        response.end("Data saved! ;)");
-    }
-    // Requête GET
-    else if (request.method == 'GET') {
-
-        // Tellement marre de logguer les requêtes de favicon que j'en a fait une! =P
-        if (request.url == "/favicon.ico") {
-            // On évite de recharger l'icône à chaque requête
-            if (favicon) {
-                response.writeHead(200, favicon.headers);
-                response.end(favicon.body);
-            } else {
-                fs.readFile(util.favicon, function (err, buf) {
-                    if (err) return next(err);
-                    favicon = {
-                        headers:{
-                            'Content-Type':'image/x-icon', 'Content-Length':buf.length, 'Cache-Control':'public, max-age=86400'
-                        },
-                        body:buf
-                    };
-                    response.writeHead(200, favicon.headers);
-                    response.end(favicon.body);
+            // Requête POST
+            if (request.method == 'POST') {
+                var postRequest;
+                var body = '';
+                request.on('data', function (data) {
+                    body += data;
                 });
-            }
-        }
+                request.on('end', function () {
+                    postRequest = qs.parse(body);
+                    util.log("Requête POST : " + postRequest);
+                    util.log(" >> Data  : " + JSON.stringify(postRequest));
+                    // Sauvegarde des données de la requête POST dans un fichier de log
+                    util.logData(request, JSON.stringify(postRequest));
 
-        else {
-
-            // Analyse de l'URL
-            var urlparts = url.parse(request.url);
-            // Décodage du chemin de l'URL
-            var params = urlparts.path.split("/");
-
-            //
-            // Cas des questions simples (ex : /?q=Est+ce+que+tu+reponds+toujours+oui(OUI/NON))
-            //
-
-            // Le contenu de la réponse à afficher (en fonction de la requête)
-            var message = queries[request.url];
-
-            // On a répondu à une question simple
-            if (message != undefined) {
-                util.log(" > Réponse : " + message);
-                // Ecriture de l'En-tête HTTP (200 : Status OK)
-                response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
-                response.end(message);
-            }
-
-            //
-            // Cas de l'énoncé 1 : Scalaskel (ex: /scalaskel/change/15)
-            //
-
-            else if ((params[1] == "scalaskel") && (params[2] == "change")) {
-                util.log("Calcul du change pour la valeur : " + params[3]);
-
-                response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
-                response.end(JSON.stringify(change.makeChange(params[3] ? params[3] : 0)));
-            }
-
-            //
-            // Cas d'un calcul bidon (ex : /?q=1+1)
-            //
-
-            else if (urlparts.query && urlparts.query.split("=")[0] == "q") {
-                var expression = urlparts.query.split("=")[1];
-                // On va tester si l'expression est bien un calcul
-                var reg = /(?:[a-z$_][a-z0-9$_]*)|(?:[;={}\[\]"'!&<>^\\?:])/ig;
-                var valid = true;
-
-                // Détection de fonction de calcul javascipt valides
-                var expr = expression.replace(reg, function ($0) {
-                    // If the name is a direct member of Math, allow
-                    if (Math.hasOwnProperty($0))
-                        return "Math." + $0;
-                    // Otherwise the expression is invalid
-                    else
-                        valid = false;
+                    // TODO : stocker les données de l'énoncé au format MarkDown (le cas échéant) dans le fichier correspondant /res/enonceX.md
                 });
 
-                if (!valid) {
-                    // Ecriture de l'En-tête HTTP (404 : Status NOT FOUND)
-                    response.writeHead(404, {"Content-Type":"text/plain;charset=utf-8"});
-                    response.end(MAUVAIS_CALCUL);
+                // Ecriture de l'En-tête HTTP (201 : Status CREATED)
+                response.writeHead(201, {"Content-Type":"text/plain;charset=utf-8"});
+                response.end("Data saved! ;)");
+            }
+            // Requête GET
+            else if (request.method == 'GET') {
+
+                // Tellement marre de logguer les requêtes de favicon que j'en a fait une! =P
+                if (request.url == "/favicon.ico") {
+                    // On évite de recharger l'icône à chaque requête
+                    if (favicon) {
+                        response.writeHead(200, favicon.headers);
+                        response.end(favicon.body);
+                    } else {
+                        fs.readFile(util.favicon, function (err, buf) {
+                            if (err) return next(err);
+                            favicon = {
+                                headers:{
+                                    'Content-Type':'image/x-icon', 'Content-Length':buf.length, 'Cache-Control':'public, max-age=86400'
+                                },
+                                body:buf
+                            };
+                            response.writeHead(200, favicon.headers);
+                            response.end(favicon.body);
+                        });
+                    }
                 }
+
                 else {
-                    var answer = 0 + eval(expr);
-                    util.log(" > Réponse : " + expression + " = " + answer);
-                    response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
-                    response.end("" + answer);
-                }
-            }
 
-            else if (false) {
-                // TODO : prendre en compte les autres types de questions et refactorer le routage une fois fini
-            }
+                    // Analyse de l'URL
+                    var urlparts = url.parse(request.url);
+                    // Décodage du chemin de l'URL
+                    var params = urlparts.path.split("/");
 
-            //
-            // Requêtes perso : afficher un énoncé, lister les accès, les données POST, les questions.
-            //
+                    //
+                    // Cas des questions simples (ex : /?q=Est+ce+que+tu+reponds+toujours+oui(OUI/NON))
+                    //
 
-            // Affichage d'un enonce
-            else if ((params[1] == "enonce") && (params[2] != "")) {
+                    // Le contenu de la réponse à afficher (en fonction de la requête)
+                    var message = queries[request.url];
 
-                var file = "enonce" + params[2] + ".md";
-                fs.readFile(__dirname + '/res/' + file, function (error, content) {
-                    if (error) {
+                    // On a répondu à une question simple
+                    if (message != undefined) {
+                        util.log(" > Réponse : " + message);
+                        // Ecriture de l'En-tête HTTP (200 : Status OK)
+                        response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+                        response.end(message);
+                    }
+
+                    //
+                    // Cas de l'énoncé 1 : Scalaskel (ex: /scalaskel/change/15)
+                    //
+
+                    else if ((params[1] == "scalaskel") && (params[2] == "change")) {
+                        util.log("Calcul du change pour la valeur : " + params[3]);
+
+                        response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+                        response.end(JSON.stringify(change.makeChange(params[3] ? params[3] : 0)));
+                    }
+
+                    //
+                    // Cas d'un calcul bidon (ex : /?q=1+1)
+                    //
+
+                    else if (urlparts.query && urlparts.query.split("=")[0] == "q") {
+                        var expression = urlparts.query.split("=")[1];
+                        // Détermination du résultat si l'expression est bien une opération mathématique valide
+                        var answer = calc.calcExpr(expression);
+
+                        if (answer != undefined) {
+                            var message = ("" + answer).replace('.', ',');
+                            util.log(" > Réponse : " + expression + " = " + message);
+                            response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+                            response.end(message);
+                        }
+                        else {
+                            // Ecriture de l'En-tête HTTP (404 : Status NOT FOUND)
+                            response.writeHead(404, {"Content-Type":"text/plain;charset=utf-8"});
+                            response.end(MAUVAIS_CALCUL);
+                        }
+                    }
+
+                    else if (false) {
+                        // TODO : prendre en compte les autres types de questions et refactorer le routage une fois fini
+                    }
+
+                    //
+                    // Requêtes perso : afficher un énoncé, lister les accès, les données POST, les questions.
+                    //
+
+                    // Affichage d'un enonce
+                    else if ((params[1] == "enonce") && (params[2] != "")) {
+
+                        var file = "enonce" + params[2] + ".md";
+                        fs.readFile(__dirname + '/res/' + file, function (error, content) {
+                            if (error) {
+                                response.writeHead(404, {"Content-Type":"text/plain;charset=utf-8"});
+                                response.end(NOT_FOUND);
+                            } else {
+                                util.log("Affichage de l'énoncé : " + file);
+                                response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+                                response.end(content);
+                            }
+                        });
+                    }
+
+                    // Affichage des infos d'accès, de données POST enregistrées ou des questions (cf. /lib/util.js)
+                    else if (routes.allowedFiles[urlparts.pathname]) {
+                        var file = routes.allowedFiles[urlparts.pathname];
+                        fs.readFile(file, function (error, content) {
+                            if (error) {
+                                response.writeHead(404, {"Content-Type":"text/plain;charset=utf-8"});
+                                response.end("Fichier non trouvé : " + file);
+                            } else {
+                                util.log("Accès privé : " + file);
+                                response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+                                response.end(content);
+                            }
+                        });
+                    }
+
+                    //
+                    // Cas par défaut : Accès Refusé sur chemin incompris
+                    //
+
+                    else {
+                        util.log("Chemin incompris !");
+                        // Ecriture de l'En-tête HTTP (404 : Status NOT FOUND)
                         response.writeHead(404, {"Content-Type":"text/plain;charset=utf-8"});
                         response.end(NOT_FOUND);
-                    } else {
-                        util.log("Affichage de l'énoncé : " + file);
-                        response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
-                        response.end(content);
                     }
-                });
-            }
-
-            // Affichage des infos d'accès, de données POST enregistrées ou des questions (cf. /lib/util.js)
-            else if (routes.allowedFiles[urlparts.pathname]) {
-                var file = routes.allowedFiles[urlparts.pathname];
-                fs.readFile(file, function (error, content) {
-                    if (error) {
-                        response.writeHead(404, {"Content-Type":"text/plain;charset=utf-8"});
-                        response.end("Fichier non trouvé : " + file);
-                    } else {
-                        util.log("Accès privé : " + file);
-                        response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
-                        response.end(content);
-                    }
-                });
-            }
-
-            //
-            // Cas par défaut : Accès Refusé sur chemin incompris
-            //
-
-            else {
-                util.log("Chemin incompris !");
-                // Ecriture de l'En-tête HTTP (404 : Status NOT FOUND)
-                response.writeHead(404, {"Content-Type":"text/plain;charset=utf-8"});
-                response.end(NOT_FOUND);
+                }
             }
         }
-    }
-});
+    )
+    ;
 
 server.listen(port);
 
