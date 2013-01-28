c0e4ad4454ca8457b08b1bf9bfec826fdb41fd05
Alain Muller <alain.muller@gmail.com>
2013-01-28 15:40:10 +0100

    Using my custom log function

--- app/lib/infix.js
+++ app/lib/infix.js
@@ -7,6 +7,7 @@
  */
 
 var bigdecimal = require("bigdecimal");
+var util = require(__dirname + '/../lib/util');
 
 // Regexp permettant de déterminer si l'expression est valide
 var validRegexp = /^([\d,]+|[\+\-\*\/\(\)]|\s+)+$/;
@@ -25,13 +26,15 @@ var opStack = [];
 var calcul = function (expression) {
     var resultat;
 
+    var nbOuvrantes = expression.match(/\(/g);
+    var nbFermantes = expression.match(/\)/g);
     // On vérifie si l'expression est valide
     if (!validRegexp.test(expression)) {
-        console.log("Expression invalide : '" + expression + "'");
+        util.log("Expression invalide : '" + expression + "'");
     }
     // On vérifie si le nombre de parenthèses est cohérent
-    else if (expression.match(/\(/g).length != expression.match(/\)/g).length) {
-        console.log("Gestion incohérente des parenthèses : " + expression.match(/\(/g).length + " ouvrantes, " + expression.match(/\)/g).length + " fermantes");
+    else if (nbOuvrantes && nbFermantes && nbOuvrantes.length != nbFermantes.length) {
+        util.log("Gestion incohérente des parenthèses : " + nbOuvrantes.length + " ouvrantes, " +nbFermantes.length + " fermantes");
     }
     // TODO : Mettre en place une meilleure regexp pour valider l'expression (gestion des opérateurs en début/fin de chaine, opérations binaires/unaires, ...)
     else {
@@ -59,8 +62,8 @@ var calcul = function (expression) {
 
             if (numRegexp.test(elem)) {
                 valStack.push(elem);
-//            console.log("On a poussé " + elem + " dans la pile valStack");
-//            console.log(" => " + JSON.stringify(valStack));
+//            util.log("On a poussé " + elem + " dans la pile valStack");
+//            util.log(" => " + JSON.stringify(valStack));
 
                 // On force le calcul si l'élément suivant n'est pas un opérateur prioritaire
                 if (elements.length > 0 && opStack.length > 0
@@ -76,7 +79,7 @@ var calcul = function (expression) {
                         break;
                     case')':
                         while (opStack[opStack.length - 1] != '(') {
-//                        console.log("Parenthèse fermante, on calcul la sous expression!");
+//                        util.log("Parenthèse fermante, on calcul la sous expression!");
                             compute();
                         }
                         // On vire la parenthèse ouvrante
@@ -90,24 +93,24 @@ var calcul = function (expression) {
                             opStack.push(elem);
                         }
                         else if (isPrioritaire(elem, opStack[opStack.length - 1])) {
-//                        console.log("Element prioritaire, on calcul la sous expression (" + elem + " VS " + opStack[opStack.length - 1] + ")");
+//                        util.log("Element prioritaire, on calcul la sous expression (" + elem + " VS " + opStack[opStack.length - 1] + ")");
                             opStack.push(elem);
                         }
                         else {
                             while (opStack.length > 0 && isPrioritaire(opStack[opStack.length - 1], elem)) {
-//                            console.log("(" + opStack.length + ") Dernier opérateur prioritaire, on calcul la sous expression (" + opStack[opStack.length - 1] + " VS " + elem + ")");
+//                            util.log("(" + opStack.length + ") Dernier opérateur prioritaire, on calcul la sous expression (" + opStack[opStack.length - 1] + " VS " + elem + ")");
                                 compute();
                             }
                             opStack.push(elem);
                         }
                         break;
                     default:
-                        console.log("Erreur : élément non supporté : '" + elem + "'");
+                        util.log("Erreur : élément non supporté : '" + elem + "'");
                         break;
                 }
 //            if (elem != ')') {
-//                console.log("On a poussé " + elem + " dans la pile opStack");
-//                console.log(" => " + JSON.stringify(opStack));
+//                util.log("On a poussé " + elem + " dans la pile opStack");
+//                util.log(" => " + JSON.stringify(opStack));
 //            }
             }
         }
@@ -129,9 +132,9 @@ var compute = function () {
     var operande1, operande2, resultat;
     var operateur;
 
-//    console.log(" > compute");
-//    console.log("  > " + JSON.stringify(valStack));
-//    console.log("  > " + JSON.stringify(opStack));
+//    util.log(" > compute");
+//    util.log("  > " + JSON.stringify(valStack));
+//    util.log("  > " + JSON.stringify(opStack));
 
     operande2 = new bigdecimal.BigDecimal(valStack.pop());
     operande1 = new bigdecimal.BigDecimal(valStack.pop());
@@ -159,7 +162,7 @@ var compute = function () {
             valStack.push(resultat.toString());
             break;
     }
-//    console.log(operande1 + ' ' + operateur + ' ' + operande2 + " = " + resultat);
+//    util.log(operande1 + ' ' + operateur + ' ' + operande2 + " = " + resultat);
 };
 
 // Fonction permettant de déterminer s'il s'agit bien d'un opérateur (pas de calcul sur les parenthèses!)
